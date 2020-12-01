e1efe4799222e849fe3e3ac57592113e4178aa3e
lucasmln <lucas.moulin78114@gmail.com>
2020-12-01 12:41:52 +0100

    linux

--- setup.sh
+++ setup.sh
@@ -2,49 +2,30 @@
 
 echo "\n#--------------------------MINIKUBE SETUP--------------------------------\n"
 
-#checking if brew is installed
-which -s brew
-if [[ $? != 0 ]] ; then
-	echo "Installing Brew..."
-	rm -rf $HOME/.brew && git clone --depth=1 https://github.com/Homebrew/brew $HOME/.brew && export PATH=$HOME/.brew/bin:$PATH && brew update && echo "export PATH=$HOME/.brew/bin:$PATH" >> ~/.zshrc &> /dev/null
-fi
-
-#checking if minikube is installed
-which -s minikube
-if [[ $? != 0 ]] ; then
-	echo -ne "\033[1;31m+>\033[0;33m Minikube installation ...\n"
-	if brew install minikube &> /dev/null
-	then
-		echo -ne "\033[1;32m+>\033[0;33m Minikube installed ! \n"
-	else
-		echo -ne "\033[1;31m+>\033[0;33m Error... During minikube installation. \n"
-		return 0
-	fi
-fi
+echo "Deleting previous cluster if there is one"
+minikube delete
 
 #Detect the platform
 OS="`uname`"
 #Change settings depending on the platform
+echo "Starting Minikube (it might take a while)"
 case $OS in
 		"Linux")
 			minikube start --vm-driver=docker --extra-config=apiserver.service-node-port-range=1-35000
-			#sed -i '' "s/192.168.99.120:5050/172.17.0.20:5050/g" src/mysql/wordpress.sql
-			#FTPS_IP=172.17.0.21
+			sed -i '' "s/192.168.99.111:5050/172.17.0.20:5050/g" srcs/mysql/wordpress.sql
+			FTPS_IP=172.17.0.21
 		;;
 		"Darwin")
 			minikube start --driver=virtualbox --extra-config=apiserver.service-node-port-range=1-35000
 			#sed -i '' "s/192.168.99.120:5050/192.168.99.120:5050/g" src/mysql/wordpress.sql
-			#FTPS_IP=192.168.99.121
+			FTPS_IP=192.168.99.129
 		;;
 		*) ;;
 esac
 
-echo "Deleting previous cluster if there is one"
-minikube delete
 
-echo "Starting Minikube (it might take a while)"
 #minikube start --vm-driver=virtualbox
-FTPS_IP=192.168.99.129
+#FTPS_IP=192.168.99.129
 eval $(minikube docker-env)
 echo "#------------------ ADDONS MINIKUBE ------------------------------"
 
@@ -60,7 +41,18 @@ kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manife
 
 #ConfigMap MetalLB
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
-kubectl apply -f srcs/yaml/metallb-configmap.yaml
+case $OS in
+                "Linux")
+                        kubectl apply -f srcs/yaml/metallb_config-Linux.yaml
+                ;;
+                "Darwin")
+                        kubectl apply -f srcs/yaml/metallb-configmap.yaml
+                ;;
+                *) ;;
+esac
+echo "Metallb enabled."
+
+#kubectl apply -f srcs/yaml/metallb-configmap.yaml
 
 kubectl apply -f srcs/yaml/mysqlvol.yaml
 kubectl apply -f srcs/yaml/influxdbvol.yaml
@@ -70,15 +62,15 @@ docker build -t nginx_i srcs/nginx/.
 kubectl apply -f srcs/yaml/mysql.yaml
 docker build -t mysql_i srcs/mysql/.
 
-docker build -t phpmyadmin_i srcs/phpmyadmin/.
+docker build -t phpmyadmin_i srcs/phpmyadmin/. #| grep "error"
 
-docker build -t wordpress_i srcs/wordpress/.
+docker build -t wordpress_i srcs/wordpress/. #| grep "error"
 
-docker build -t service_ftps --build-arg IP=${FTPS_IP} srcs/ftps
+docker build -t service_ftps --build-arg IP=${FTPS_IP} srcs/ftps #| grep "error"
 
-docker build -t influxdb_i srcs/influxdb/.
+docker build -t influxdb_i srcs/influxdb/. #| grep "error"
 
-docker build -t grafana_i srcs/grafana/.
+docker build -t grafana_i srcs/grafana/. #| grep "error"
 
 echo "\n#----------------------------------- SETUP K8s ----------------------------\n"
 kubectl apply -f srcs/yaml/ftps.yaml
