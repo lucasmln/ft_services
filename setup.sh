0fb5edd91effe93155ea997be4a48e4034827134
lmoulin <lucas.moulin78114@gmail.com>
2020-12-08 12:12:56 +0100

    ok

--- setup.sh
+++ setup.sh
@@ -22,9 +22,9 @@ case $OS in
 esac
 CLUSTER_IP="$(kubectl get node -o=custom-columns='DATA:status.addresses[0].address' | sed -n 2p)"
 echo $CLUSTER_IP
-sed -i '' "s/"192.168.99.253"/"$CLUSTER_IP"/g" srcs/yaml/metallb-configmap.yaml
-sed -i '' "s/"192.168.99.253"/"$CLUSTER_IP"/g" srcs/nginx/default.conf
-sed -i '' "s/"192.168.99.253"/"$CLUSTER_IP"/g" srcs/mysql/wordpress.sql
+sed -i "" "s/"192.168.99.253"/"$CLUSTER_IP"/g" srcs/yaml/metallb-configmap.yaml
+sed -i "" "s/"192.168.99.253"/"$CLUSTER_IP"/g" srcs/nginx/default.conf
+sed -i "" "s/"192.168.99.253"/"$CLUSTER_IP"/g" srcs/mysql/wordpress.sql
 echo "Starting Minikube (it might take a while)"
 ~cat srcs/yaml/metallb-configmap.yaml
 #minikube start --vm-driver=virtualbox
